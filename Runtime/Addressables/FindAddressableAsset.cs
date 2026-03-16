8e3ba459d252769fc13746dafa490433c63ce062
= <matthew.r.walker@gmail.com>
2026-03-16 16:40:18 -0400

    Re-add missing FindAddressableAsset method

--- Runtime/Addressables/FindAddressableAsset.cs
+++ Runtime/Addressables/FindAddressableAsset.cs
@@ -6,13 +6,14 @@ using UnityEditor;
 using UnityEditor.AddressableAssets;
 using UnityEditor.AddressableAssets.Settings;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace MWUtilityScripts.Addressables
 {
     public static class FindAddressableAsset
     {
-        private static readonly Dictionary<string, AsyncOperationHandle<Object>> _guidToHandle = new();
+        private static readonly Dictionary<string, AsyncOperationHandle<Object>> GuidToHandle = new();
 
         public static SerializedObject FindAddressableSerializedObject(SerializedObject input)
         {
@@ -28,22 +29,37 @@ namespace MWUtilityScripts.Addressables
             )
                 return input;
             var guid = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(input.targetObject));
-            if (_guidToHandle.TryGetValue(guid, out var handle) && handle.IsValid() && handle.Result != null)
+            if (GuidToHandle.TryGetValue(guid, out var handle) && handle.IsValid() && handle.Result != null)
             {
                 return new SerializedObject(handle.Result);
             }
             else
             {
                 handle = UnityEngine.AddressableAssets.Addressables.LoadAssetAsync<Object>(guid);
-                _guidToHandle[guid] = handle;
+                GuidToHandle[guid] = handle;
                 var asset = handle.WaitForCompletion();
                 return new SerializedObject(asset);
             }
         }
+        
+#if HAS_UNITASK
+        public static async UniTask<T> GetAssetBundleInstance<T>(T obj)
+            where T : Object
+        {
+            if (!EditorApplication.isPlaying)
+                return obj;
+            var assetEntry = GetAddressableAssetEntry(obj);
+            if (assetEntry == null)
+                return obj;
 
+            var reference = new AssetReferenceT<T>(assetEntry.guid);
+            return await reference.LoadAssetAsync<T>();
+        }
+#endif
+        
         private static AddressableAssetEntry GetAddressableAssetEntry(Object obj)
         {
-            AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
+            var settings = AddressableAssetSettingsDefaultObject.Settings;
             return settings.FindAssetEntry(AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(obj)), true);
         }
     }
