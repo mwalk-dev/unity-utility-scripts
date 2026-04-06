c8e8ddd2da832671b414468a53dc6a13b249f0cf
= <matthew.r.walker@gmail.com>
2026-04-06 14:33:37 -0400

    Nullability in MonoBehaviourExtensions

--- Runtime/UnityExtensions/MonoBehaviourExtensions.cs
+++ Runtime/UnityExtensions/MonoBehaviourExtensions.cs
@@ -1,3 +1,4 @@
+#nullable enable
 using System;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -27,7 +28,7 @@ namespace MWUtilityScripts.UnityExtensions
         /// <exception cref="ArgumentException"></exception>
         public static void AssignComponent<TSource, TMember>(
             this TSource mb,
-            Expression<Func<TSource, TMember>> lambda,
+            Expression<Func<TSource, TMember?>> lambda,
             bool throwIfNotFound = false
         )
             where TSource : MonoBehaviour
@@ -41,65 +42,72 @@ namespace MWUtilityScripts.UnityExtensions
             var memberInfo = GetMemberInfo(lambda);
             var (name, currentValue, type, setter) = memberInfo switch
             {
-                FieldInfo fieldInfo
-                    => (
-                        fieldInfo.Name,
-                        fieldInfo.GetValue(mb),
-                        fieldInfo.FieldType,
-                        (Action<Component>)(cmp => fieldInfo.SetValue(mb, cmp))
-                    ),
-                PropertyInfo propertyInfo
-                    => (
-                        propertyInfo.Name,
-                        propertyInfo.GetValue(mb),
-                        propertyInfo.PropertyType,
-                        cmp => propertyInfo.SetValue(mb, cmp)
-                    ),
-                _ => throw new ArgumentException($"Expression '{lambda}' did not return a field or property"),
+                FieldInfo fieldInfo => (
+                    fieldInfo.Name,
+                    fieldInfo.GetValue(mb),
+                    fieldInfo.FieldType,
+                    (Action<Component>)(cmp => fieldInfo.SetValue(mb, cmp))
+                ),
+                PropertyInfo propertyInfo => (
+                    propertyInfo.Name,
+                    propertyInfo.GetValue(mb),
+                    propertyInfo.PropertyType,
+                    cmp => propertyInfo.SetValue(mb, cmp)
+                ),
+                _ => throw new ArgumentException(
+                    $"Expression '{lambda}' did not return a field or property"
+                ),
             };
-            if (currentValue == null)
+            if (currentValue != null)
             {
-                var components = mb.transform.root.GetComponentsInChildren(type);
-                if (components.Length == 1)
+                return;
+            }
+            var components = mb.transform.root.GetComponentsInChildren(type);
+            if (components.Length == 1)
+            {
+                // Unity doesn't allow saving prefabs from inside OnValidate, which is often when we want to use
+                // this function. So we defer execution of the save to make it permissible.
+                EditorApplication.delayCall += () =>
                 {
-                    // Unity doesn't allow saving prefabs from inside OnValidate, which is often when we want to use
-                    // this function. So we defer execution of the save to make it permissible.
-                    EditorApplication.delayCall += () =>
-                    {
-                        // Since we delayed after finding the component, something might have happened
-                        if (components[0] == null)
-                            return;
-                        setter(components[0]);
-                        Debug.Log($"{nameof(AssignComponent)} successfully assigned {components[0].name} to {mb.name}");
-                        Undo.RecordObject(mb, "TryAssign");
-                        EditorUtility.SetDirty(mb);
-                        AssetDatabase.SaveAssetIfDirty(mb);
-                    };
-                }
-                else if (components.Length > 1)
-                {
-                    Debug.LogError(
-                        $"{mb.name} has {components.Length} components of type {name} under it and {nameof(AssignComponent)} expects at most one, so no reference was automatically assigned."
+                    // Since we delayed after finding the component, something might have happened
+                    if (components[0] == null)
+                        return;
+                    setter(components[0]);
+                    Debug.Log(
+                        $"{nameof(AssignComponent)} successfully assigned {components[0].name} to {mb.name}"
                     );
-                }
-                else if (throwIfNotFound)
-                {
-                    Debug.LogError(
-                        $"{mb.name} has no components of type {name} under it and {nameof(AssignComponent)} expected exactly one!"
-                    );
-                }
+                    Undo.RecordObject(mb, "TryAssign");
+                    EditorUtility.SetDirty(mb);
+                    AssetDatabase.SaveAssetIfDirty(mb);
+                };
+            }
+            else if (components.Length > 1)
+            {
+                Debug.LogError(
+                    $"{mb.name} has {components.Length} components of type {name} under it and {nameof(AssignComponent)} expects at most one, so no reference was automatically assigned."
+                );
+            }
+            else if (throwIfNotFound)
+            {
+                Debug.LogError(
+                    $"{mb.name} has no components of type {name} under it and {nameof(AssignComponent)} expected exactly one!"
+                );
             }
 #endif
         }
 
-        private static MemberInfo GetMemberInfo<TSource, TProperty>(Expression<Func<TSource, TProperty>> memberLambda)
+        private static MemberInfo GetMemberInfo<TSource, TProperty>(
+            Expression<Func<TSource, TProperty>> memberLambda
+        )
         {
             if (memberLambda.Body is not MemberExpression member)
             {
-                throw new ArgumentException($"Expression '{memberLambda}' refers to a method, not a property.");
+                throw new ArgumentException(
+                    $"Expression '{memberLambda}' refers to a method, not a property."
+                );
             }
 
-            Type type = typeof(TSource);
+            var type = typeof(TSource);
             if (
                 member.Member.ReflectedType != null
                 && type != member.Member.ReflectedType
