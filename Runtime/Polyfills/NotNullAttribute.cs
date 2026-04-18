62995fc3e669ea2317a5f028e8132d6cbdfc948c
= <matthew.r.walker@gmail.com>
2026-04-18 16:01:48 -0400

    Remove unnecessary polyfill

--- Runtime/Polyfills/NotNullAttribute.cs
+++
@@ -1,8 +0,0 @@
-#nullable enable
-using System;
-
-namespace System.Diagnostics.CodeAnalysis
-{
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
-    public sealed class NotNullAttribute : Attribute { }
-}
