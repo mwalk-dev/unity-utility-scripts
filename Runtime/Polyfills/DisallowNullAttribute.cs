64b2bc8581e93fa917acc291b8bde8e441c3808c
= <matthew.r.walker@gmail.com>
2026-05-02 08:36:38 -0400

    Drop nullability polyfill support

--- Runtime/Polyfills/DisallowNullAttribute.cs
+++
@@ -1,8 +0,0 @@
-#nullable enable
-using System;
-
-namespace System.Diagnostics.CodeAnalysis
-{
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter | AttributeTargets.ReturnValue, Inherited = false)]
-    public sealed class DisallowNullAttribute : Attribute { }
-}
